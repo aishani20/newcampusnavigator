da4bccf821e7de4b6abd7d3bac5bb19d4c3d9056
abtyagi15 <abtyagi150702@gmail.com>
2024-03-02 22:50:51 +0530

    for changing state the signup context is used

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/common/Navbar";
+import SignupForm from "../components/core/SignupPage/SignupForm";  
+import Footer from "../components/common/Footer";
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="h-screen">
+      <div>
+        <div>
+          <Navbar />
+        </div>
+        <div>
+          <SignupForm />
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
