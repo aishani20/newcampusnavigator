4f94e25f587598efaef4a7108445aa7c972b0b39
abtyagi15 <abtyagi150702@gmail.com>
2024-03-06 21:28:56 +0530

    console.log is commented

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -22,7 +22,7 @@ const mailSender = async (email, title, body) => {
       html: `${body}`,
     });
 
-    console.log(info);
+    // console.log(info);
     return info;
   } catch (e) {
     console.error(e);
