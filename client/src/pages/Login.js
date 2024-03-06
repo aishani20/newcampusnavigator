d41f2918498bc19b04fff9c2d9cdab010ec39a14
abtyagi15 <abtyagi150702@gmail.com>
2024-03-06 20:21:55 +0530

    navbar folder structure is changed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/common/Navbar'
+import Navbar from '../components/common/Navbar/Navbar'
 import Footer from '../components/common/Footer'
 import LoginForm from '../components/core/LoginPage/LoginForm'
 const Login = () => {
