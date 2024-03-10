4921a96ba186b8083875eb98599215b7e2284002
abtyagi15 <abtyagi150702@gmail.com>
2024-03-10 17:16:05 +0530

    token authenticaion for login

---
+++ server/middlewares/auth.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+exports.auth = async (req, res, next) => {
+  try {
+    console.log("Before token authorisation");
+
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorisation").replace("Bearer ", "");
+    console.log("Token extraction");
+
+    if (!token) {
+      res.json({
+        success: false,
+        message: "Token is missing",
+      });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        status: false,
+        message: "Token is invalid or has expired",
+      });
+    }
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      status: false,
+      message: "Error in the auth middleware",
+    });
+  }
+};
