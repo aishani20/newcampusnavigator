1ad8548d279e234b3e691a63e6f4eb0c80d5e2f7
abtyagi15 <abtyagi150702@gmail.com>
2024-03-21 00:07:20 +0530

    Navbar and Footer components shifted to App.js

--- client/src/pages/Predict.js
+++ client/src/pages/Predict.js
@@ -1,21 +1,11 @@
 import React from "react";
-import Navbar from "../components/common/Navbar/Navbar";
-import Footer from "../components/common/Footer";
 import PredictForm from "../components/core/PredictPage/PredictionForm";
 
 const Predict = () => {
   return (
     <div className="h-screen">
       <div>
-        <div>
-          <Navbar />
-        </div>
-        <div>
-          <PredictForm />
-        </div>
-        <div>
-          <Footer />
-        </div>
+        <PredictForm />
       </div>
     </div>
   );
