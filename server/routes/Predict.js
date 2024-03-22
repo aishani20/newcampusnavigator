edfd04917234ec496e6ac256f894c63cffc54e42
abtyagi15 <abtyagi150702@gmail.com>
2024-03-23 00:56:23 +0530

    auth middleware is added

--- server/routes/Predict.js
+++ server/routes/Predict.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const {auth} = require('../middlewares/auth');
 const {predict} = require('../controllers/Prediction');
 
-router.post('/predict',predict);
+router.post('/predict',auth,predict);
 
 module.exports = router;
