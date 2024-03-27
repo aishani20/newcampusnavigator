50ba16da694c12c4e39765b24e03f7d32877773e
abtyagi15 <abtyagi150702@gmail.com>
2024-03-28 05:28:30 +0530

    route for reset password is added

--- server/routes/User.js
+++ server/routes/User.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { signup,verifyOtp,login } = require('../controllers/Auth');
+const { resetPasswordToken } = require('../controllers/ResetPassword');
 
 router.post('/signup',signup);
 router.post('/verify-email',verifyOtp);
 router.post('/login',login);
+
+
+//Reset Password
+router.post('/reset-password',resetPasswordToken);
 module.exports = router;
