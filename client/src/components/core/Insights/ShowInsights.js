f6f496c8b78f7c60da21cc6bf3e50dcd7cf71c6e
abtyagi15 <abtyagi150702@gmail.com>
2024-04-12 23:51:03 +0530

    fetch is replaced with axios to get the insights

--- client/src/components/core/Insights/ShowInsights.js
+++ client/src/components/core/Insights/ShowInsights.js
@@ -1,39 +1,30 @@
 import React, { useState, useEffect } from "react";
 import InsightCard from "./InsightCard";
+import axios from "axios";
 
 const ShowInsights = () => {
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
   const [insights, setInsights] = useState([]);
 
   useEffect(() => {
-    async function getInsights() {
-      const response = await fetch(`${backendUrl}/getAllInsights`, {
-        method: "GET",
-        credentials: "include",
-      });
-      const data = await response.json();
-      const insightsArray = data.insights;
-      console.log(
-        "Insights recieved from backend",
-        insightsArray[0],
-        typeof insightsArray
-      );
+    axios
+      .get(`${backendUrl}/getAllInsights`, { withCredentials: true })
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        const insightsArray = data.insights;
 
-      setInsights(insightsArray);
-    }
-    getInsights();
+        //updating the insights state
+        setInsights(insightsArray);
+      })
+      .catch((err) => console.log(err));
   }, [backendUrl]);
-  // const newInsightArray = Object.values(insights);
-  // console.log("Insights", insights,typeof(newInsightArray[0]);
+
   return (
     <div>
-    {
-      Object.values(insights).map((insight) => {
-        return (
-          <InsightCard key={insight._id} insight={insight} />
-        )
-      })
-    }
+      {Object.values(insights).map((insight) => {
+        return <InsightCard key={insight._id} insight={insight} />;
+      })}
     </div>
   );
 };
