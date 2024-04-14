3940296abff9a5b53d732cb004a04f74484b0a05
abtyagi15 <abtyagi150702@gmail.com>
2024-04-15 01:34:44 +0530

    updated, token from req.header

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -106,27 +106,27 @@ exports.login = async (req, res) => {
     }
     if (await bcrypt.compare(password, user.password)) {
       const token = jwt.sign(
-        { email: user.email, id: user._id},
+        { id: user._id },
         process.env.JWT_SECRET,
         {
           expiresIn: "24h",
         }
-      )
+      );
 
       // Save token to user document in database
-      user.token = token
-      user.password = undefined
+      user.token = token;
+      user.password = undefined;
       // Set cookie for token and return success response
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-      }
+      };
       res.cookie("token", token, options).status(200).json({
         success: true,
         token,
         user,
         message: `User Login Success`,
-      })
+      });
     }
   } catch (e) {
     console.error(e);
@@ -136,4 +136,3 @@ exports.login = async (req, res) => {
     });
   }
 };
-
