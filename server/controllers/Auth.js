3129d21682c4bc1b15d020e123873e63e5307205
abtyagi15 <abtyagi150702@gmail.com>
2024-04-14 18:59:51 +0530

    changes in token jwt.sign

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -105,19 +105,27 @@ exports.login = async (req, res) => {
       });
     }
     if (await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "24h",
-      });
-      user.token = token;
+      const token = jwt.sign(
+        { email: user.email, id: user._id},
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
+        }
+      )
 
+      // Save token to user document in database
+      user.token = token
+      user.password = undefined
+      // Set cookie for token and return success response
       const options = {
-        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        // httpOnly: true,
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
       }
-      return res.cookie('token',token,options).json({
+      res.cookie("token", token, options).status(200).json({
         success: true,
-        token: token,
-        message: "Logged in successfully"
+        token,
+        user,
+        message: `User Login Success`,
       })
     }
   } catch (e) {
