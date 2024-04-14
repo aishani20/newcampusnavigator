fbe7509f8feab5aaedc09246e3d84c40faab0fc1
abtyagi15 <abtyagi150702@gmail.com>
2024-04-15 04:20:13 +0530

    faqs, questions modified

--- client/src/components/core/HomePage/FAQHome.js
+++ client/src/components/core/HomePage/FAQHome.js
@@ -3,7 +3,7 @@
 
 // const FAQHome = () => {
 //   return (
-    
+
 //     <div className="mx-auto pl-4 my-16">
 //       <h2 className="text-xl font-bold mb-4">Frequently Asked Questions</h2>
 
@@ -52,27 +52,28 @@ import { FaPlus, FaMinus } from "react-icons/fa";
 const FAQHome = () => {
   const faqs = [
     {
-      question: "What is the Placement Management System?",
-      answer: "The Placement Management System is a software platform designed to streamline and manage the placement process in educational institutions. It helps automate tasks such as job postings, student registrations, interview scheduling, and placement analytics.",
+      question: "What is CampusNavigator?",
+      answer:
+        "The Placement Management System is a software platform designed to streamline and manage the placement process in educational institutions. It helps automate tasks such as job postings, student registrations, interview scheduling, and placement analytics.",
     },
     {
-      question: "How is Placement Software helpful for higher education institutions?",
-      answer: "Placement Software offers several benefits to higher education institutions. It improves efficiency by automating manual tasks, enhances communication between students and recruiters, provides data insights for better decision-making, and contributes to overall placement success rates.",
+      question: "How is CampusNavigator helpfull for you?",
+      answer:
+        "Placement Software offers several benefits to higher education institutions. It improves efficiency by automating manual tasks, enhances communication between students and recruiters, provides data insights for better decision-making, and contributes to overall placement success rates.",
     },
     {
-      question: "What are the main features of Placement Software?",
-      answer: "Key features of Placement Software include job posting and application management, resume database, interview scheduling, analytics and reporting, communication tools, and integration capabilities with other educational modules.",
-    },
-    {
-      question: "Can the Creatrix Placement Management System be customizable and integrable with other education modules?",
-      answer: "Yes, the Creatrix Placement Management System is designed to be highly customizable to suit the specific needs of educational institutions. It offers integration capabilities with existing educational modules such as student information systems, learning management systems, and more.",
+      question: "What are the main features of CampusNavigator?",
+      answer:
+        "Key features of Placement Software include job posting and application management, resume database, interview scheduling, analytics and reporting, communication tools, and integration capabilities with other educational modules.",
     },
   ];
 
   const [isOpen, setIsOpen] = useState(Array(faqs.length).fill(false)); // Initialize isOpen state for FAQ items
 
   const toggleFAQ = (index) => {
-    const updatedOpenState = isOpen.map((item, i) => (i === index ? !item : false));
+    const updatedOpenState = isOpen.map((item, i) =>
+      i === index ? !item : false
+    );
     setIsOpen(updatedOpenState);
   };
 
@@ -83,7 +84,7 @@ const FAQHome = () => {
       <div className="space-y-4">
         {faqs.map((faq, index) => (
           <div key={index}>
-            <div className="flex items-center justify-between border-b pb-2">
+            <div className="flex items-center justify-between border-b pb-2 cursor-pointer" onClick={()=>toggleFAQ(index)}>
               <p className="text-lg">{faq.question}</p>
               {isOpen[index] ? (
                 <FaMinus
@@ -112,6 +113,3 @@ const FAQHome = () => {
 };
 
 export default FAQHome;
-
-
-
