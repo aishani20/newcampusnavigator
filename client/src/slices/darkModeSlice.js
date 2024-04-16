8b019a38a964728c33f4f9dea49f97bb33597f9e
abtyagi15 <abtyagi150702@gmail.com>
2024-04-16 20:53:09 +0530

    darkmode related

---
+++ client/src/slices/darkModeSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    darkMode: localStorage.getItem("darkMode") ? JSON.parse(localStorage.getItem("darkMode")) : true
+}
+
+const darkModeSlice = createSlice({
+    name: "darkMode",
+    initialState: initialState,
+    reducers: {
+        setDarkMode: (state, value) => {
+            state.darkMode = value.payload;
+        }
+    }
+})
+
+export const { setDarkMode } = darkModeSlice.actions;
+export default darkModeSlice.reducer;
