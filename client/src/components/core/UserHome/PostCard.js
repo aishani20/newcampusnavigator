ded821a1d9872bb6c7ea4fa686eaec90057b0eff
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2024-04-18 03:15:17 +0530

    Updated the Dark Mode

--- client/src/components/core/UserHome/PostCard.js
+++ client/src/components/core/UserHome/PostCard.js
@@ -12,13 +12,13 @@ const PostCard = ({ postcard }) => {
     setShow(!show);
   }
   return (
-    <div className="mx-auto my-4 bg-white border border-gray-200 p-6 rounded-md max-w-3xl shadow-sm hover:bg-gray-100">
+    <div className="mx-auto my-4 bg-white border border-gray-200 p-6 rounded-md max-w-3xl shadow-sm hover:bg-gray-100 dark:bg-[#1E2023] dark:hover:bg-[#333436] dark:border-[#403B3D]">
       <div className="items-center mb-4 flex">
-        <VscAccount className="w-10 h-10 rounded-md object-cover mr-2" />
+        <VscAccount className="w-10 h-10 rounded-md object-cover mr-2 dark:text-white" />
         <div className="flex flex-col w-full">
-          <div className="font-bold text-lg">{postcard.heading}</div>
+          <div className="font-bold text-lg dark:text-white">{postcard.heading}</div>
           <div className="text-gray-600 flex justify-between items-center">
-            <div className="text-sm">{postcard.author}</div>
+            <div className="text-sm dark:text-[#8C8880]">{postcard.author}</div>
             <div className="flex items-center ml-4 relative">
               <p className="text-sm text-gray-500 mr-2">
                 {postcard.creationDate}
@@ -56,18 +56,18 @@ const PostCard = ({ postcard }) => {
           </div>
         </div>
       </div>
-      <div className="mb-6">{postcard.description}</div>
+      <div className="mb-6 dark:text-white">{postcard.description}</div>
       <div className="flex items-center text-gray-600 text-sm">
-        <button className="flex items-center hover:text-blue-600">
-          <SlLike className="mr-1.5" />
+        <button className="flex items-center hover:text-blue-600 dark:text-[#8C8880]">
+          <SlLike className="mr-1.5 dark:text-[#8C8880]" />
           Like
         </button>
-        <button className="flex items-center ml-4 hover:text-green-600">
-          <LiaCommentAlt className="mr-1.5" />
+        <button className="flex items-center ml-4 hover:text-green-600 dark:text-[#8C8880]">
+          <LiaCommentAlt className="mr-1.5 dark:text-[#8C8880]" />
           Comment
         </button>
-        <button className="flex items-center ml-4 hover:text-indigo-600">
-          <SlShareAlt className="mr-1.5" />
+        <button className="flex items-center ml-4 hover:text-indigo-600 dark:text-[#8C8880]">
+          <SlShareAlt className="mr-1.5 dark:text-[#8C8880]" />
           Share
         </button>
       </div>
