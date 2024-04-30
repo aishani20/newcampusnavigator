bb7ed36dda5e2c885fcac468545e19b8ccc4d83e
abtyagi15 <abtyagi150702@gmail.com>
2024-04-30 19:59:58 +0530

    changed design and added responsiveness

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,63 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assests/LogoImage.png";
-import CurvedLine from "../../assests/curveUnderline.svg";
-import ForwardArrow from "../../assests/ForwardArrow.svg";
+import LightCurvedLine from "../../assests/curveUnderline.svg";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import DarkCurvedLine from "../../assests/darkCurveUnderline.png";
 import { useSelector } from "react-redux";
-
+import { toast } from "react-hot-toast";
 
 const Footer = () => {
-  const {darkMode} = useSelector((state) => state.darkMode);
+  const { darkMode } = useSelector((state) => state.darkMode);
+  const [email, setEmail] = useState(null);
+  const { user } = useSelector((state) => state.profile);
+  const subscribeHandler = () => {
+    toast.success("Subscribed Successfully");
+    setEmail("");
+  };
+
   return (
     <div className="max-w-[1292px] flex flex-col mx-auto mb-5 ">
-      <div className="relative montserrat h-[26.34px]">
-        <div className="flex item-center absolute gap-[1px]">
-          <img src={Logo} alt="Logo" className="w-[27.16px]" />
-          <div className="text-[19.11px] font-bold">
-            <span className="dark:text-white">Campus</span><span className="text-[#3652DD]">Navigator</span>
+      <div className="montserrat">
+        <div className="relative">
+          <div className="flex item-center absolute">
+            <img src={Logo} alt="Logo" className="w-7" />
+            <div className="font-bold text-lg">
+              <span className="dark:text-white">Campus</span>
+              <span className="text-[#3652DD]">Navigator</span>
+            </div>
+          </div>
+          <div className="absolute w-48 top-4">
+            <img
+              src={!darkMode ? LightCurvedLine : DarkCurvedLine}
+              alt="CurvedLine"
+            />
           </div>
         </div>
-        <div className="absolute w-[205px] top-[15px]">
-          <img src={!darkMode ? CurvedLine : DarkCurvedLine} alt="CurvedLine" />
+
+        <div className="flex items-center gap-2 dark:text-[#C5C4C2] justify-end mt-2">
+          <FaInstagramSquare className="w-6 h-6 text-[#F71F2B]" />{" "}
+          <FaSquareXTwitter className="w-6 h-6 text-[#000000] dark:text-white" />{" "}
+          <FaLinkedin className="w-6 h-6 text-[#0073B1]" />
         </div>
       </div>
-      <div className="grid md:grid-cols-2 grid-rows-2">
-        <div className="flex flex-col dark:text-[#C5C4C2]">
-          <b>+91 9340204054</b>
-          <div>abtyagi150702@gmail.com</div>
+      <div className="flex mt-7 md:flex-row flex-col">
+        <div className="flex md:w-3/5 md:border-none border-2 border-dashed">
+          <ul className="md:border-l-2 border-dashed p-2 mx-2">
+            <li>
+              <a
+                href="/#"
+                target="_blank"
+                className="dark:text-[#C5C4C2] hover:bg-[#2b2b2b0a] dark:hover:bg-[#5c5b5b] px-2 py-1"
+              >
+                Progress Report
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#"
+                target="_blank"
+                className="dark:text-[#C5C4C2] hover:bg-[#2b2b2b0a] dark:hover:bg-[#5c5b5b] px-2 py-1"
+              >
+                Project Poster
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#"
+                target="_blank"
+                className="dark:text-[#C5C4C2] hover:bg-[#2b2b2b0a] dark:hover:bg-[#5c5b5b] px-2 py-1"
+              >
+                Project PPT
+              </a>
+            </li>
+          </ul>
+          <ul className="md:border-l-2 border-dashed p-2 mx-2">
+            <li>
+              <a
+                href="/#"
+                target="_blank"
+                className="dark:text-[#C5C4C2] hover:bg-[#2b2b2b0a] dark:hover:bg-[#5c5b5b] px-2 py-1"
+              >
+                Institute Website
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#"
+                target="_blank"
+                className="dark:text-[#C5C4C2] hover:bg-[#2b2b2b0a] dark:hover:bg-[#5c5b5b] px-2 py-1"
+              >
+                Technical Club FLUX
+              </a>
+            </li>
+            <li>
+              <a
+                href="/#"
+                target="_blank"
+                className="dark:text-[#C5C4C2] hover:bg-[#2b2b2b0a] dark:hover:bg-[#5c5b5b] px-2 py-1"
+              >
+                Training and Placement cell
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="bg-[#3652DD] flex md:flex-row flex-col md:items-center rounded-[10px] 
-                      text-white px-[42px] md:justify-around items-start dark:bg-[#1B37C0] ">
-          <div className="text-[18px] font-bold">
-            Subscribe to Our Newsletter
-          </div>
-          <div className="bg-white flex justify-between rounded-[6px] ">
-            <div className="text-[14px]  text-[#0A142F]">Email Address</div>
-            <img
-              src={ForwardArrow}
-              alt="ForwardArrow"
-              className=" bg-[#D6D6D6] rounded-r-[6px] "
+
+        <div className="flex flex-col sm:gap-1 gap-3 md:w-2/5 my-3">
+          {user?.userRole !== "major" && (
+            <div className="flex flex-col dark:text-[#C5C4C2]">
+              <b>+91 9340204054</b>
+              <p>abtyagi150702@gmail.com</p>
+            </div>
+          )}
+
+          <div className="flex gap-2">
+            <input
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Enter Email Address"
+              className="py-1 px-3 border rounded-md"
+              onChange={(e) => setEmail(e.target.value)}
             />
+            <button
+              className="bg-[#3652DD] py-1 px-4 rounded-md text-white"
+              onClick={subscribeHandler}
+            >
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
       <hr className="border my-4 dark:border-[#51585B]" />
-      <div className="flex md:flex-row flex-col items-center md:justify-between md:items-center">
-        <div className="flex items-center gap-3 dark:text-[#C5C4C2]">
-          <span>Social Media</span>
-          <span className="flex">
-            <FaInstagramSquare /> <FaSquareXTwitter /> <FaLinkedin />
-          </span>
+      <div className="flex sm:flex-row flex-col sm:justify-between sm:items-center">
+        <div className="flex sm:gap-4 dark:text-[#C5C4C2] sm:flex-row flex-col">
+          <div className="flex gap-4">
+            <span>A product of</span>
+            <img src={Logo} alt="Logo" className="w-[26.16px]" />
+          </div>
+          <div>
+            © 2024
+            {`${user?.userRole === "major" ? " CS2024TID16" : " ABTYAGI"}`}. All
+            rights reserved
+          </div>
         </div>
-        <div className="flex gap-4 dark:text-[#C5C4C2]">
-          <span>A product of</span>
-          <img src={Logo} alt="Logo" className="w-[26.16px]" />
-        </div>
-        <div className="dark:text-[#C5C4C2]">
-        <div>© 2024 ABTYAGI. All rights reserved</div>
+        <div className="flex gap-2">
+          <p className="cursor-pointer dark:text-[#C5C4C2] underline">
+            Privacy Policy
+          </p>
+          <p className="cursor-pointer dark:text-[#C5C4C2] underline">
+            Sitemap
+          </p>
         </div>
       </div>
     </div>
