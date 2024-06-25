e854ba7c95648f1c3a613ee2aee1df1df630d79c
abtyagi15 <abtyagi150702@gmail.com>
2024-06-26 03:15:26 +0530

    daily target adding request

--- server/controllers/CompaniesApply.js
+++ server/controllers/CompaniesApply.js
@@ -1,6 +1,13 @@
+const AppliedCompanies = require("../models/AppliedCompanies");
+
 exports.dailyTarget = async (req, res) => {
     try{
-        
+        const { dailyTarget } = req.body;
+        await AppliedCompanies.create({dailyTarget: dailyTarget, user: req.user.id });
+
+        res.status(200).json({
+            message: "Daily target updated successfully",
+        });
     }
     catch(err){
         console.log(err);
