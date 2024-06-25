f8122aeab61912a5b6e43fb1650ea2e768de202d
abtyagi15 <abtyagi150702@gmail.com>
2024-06-26 04:37:31 +0530

    get request handling

--- server/controllers/CompaniesApply.js
+++ server/controllers/CompaniesApply.js
@@ -1,15 +1,36 @@
 const AppliedCompanies = require("../models/AppliedCompanies");
 
 exports.dailyTarget = async (req, res) => {
-    try{
-        const { dailyTarget } = req.body;
-        await AppliedCompanies.create({dailyTarget: dailyTarget, user: req.user.id });
+  try {
+    const { dailyTarget } = req.body;
+    await AppliedCompanies.create({
+      dailyTarget: dailyTarget,
+      user: req.user.id,
+    });
 
-        res.status(200).json({
-            message: "Daily target updated successfully",
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
+    res.status(200).json({
+      message: "Daily target updated successfully",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.appliedCompaniesData = async (req, res) => {
+  try {
+    const { currentDate } = req.headers;
+    const data = await AppliedCompanies.find({
+      user: req.user.id && appliedDate < currentDate,
+    });
+    console.log("current date", currentDate);
+    console.log("current date calculated in backend", Date.now());
+    console.log("data", data);
+
+    res.status(200).json({
+      message: "Applied companies data fetched successfully",
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
