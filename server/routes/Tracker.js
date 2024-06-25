4d169266e77ec15338876fda5e3b8c971c2c4e65
abtyagi15 <abtyagi150702@gmail.com>
2024-06-26 04:34:09 +0530

    added route for getting applied companies data

--- server/routes/Tracker.js
+++ server/routes/Tracker.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const {auth} = require('../middlewares/auth');
-const {dailyTarget} = require('../controllers/CompaniesApply');
+const {dailyTarget, appliedCompaniesData} = require('../controllers/CompaniesApply');
 router.post('/daily-target',auth, dailyTarget);
+router.get('/applied-companies',auth, appliedCompaniesData);
 
 module.exports = router;
