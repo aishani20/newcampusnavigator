cdfe8646399455dcb9b1e6786e1eb152c01c2531
abtyagi15 <abtyagi150702@gmail.com>
2024-06-28 21:28:15 +0530

    params are added, props passed to child component

--- client/src/components/core/tracker/AppliedCompanies/AppliedCompanies.js
+++ client/src/components/core/tracker/AppliedCompanies/AppliedCompanies.js
@@ -3,56 +3,76 @@ import { IoIosArrowDown } from "react-icons/io";
 import PlusIcon from "../../../../assests/tracker/daily_target_plus.png";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import CompanyDetails from "./CompanyDetails";
+import ISOCurrentDateFormatter from "../../../../utils/ISOCurrentDateFormatter";
 
 const AppliedCompanies = () => {
   const [dailyTarget, setDailyTarget] = useState(0);
+  const [companyDetails, setCompanyDetails] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(ISOCurrentDateFormatter());
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
   const { token } = useSelector((state) => state.auth);
 
   //updating the value of daily target in the database
   const sendDailyTarget = async () => {
     console.log("Before sending request to backend");
-    const response = await axios.post(
-      `${backendUrl}/daily-target`,
-      dailyTarget,
-      {
+    await axios
+      .post(`${backendUrl}/daily-target`, dailyTarget, {
         headers: {
           Authorisation: `Bearer ${token}`,
         },
-      }
-    );
-    const data = await response.data;
-    console.log(data);
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
-    const currentDate = new Date();
-    const isoStringCurrentDay = currentDate.toISOString();
-    let specificDate = currentDate;
-    specificDate.setUTCHours(0, 0, 0, 0);
-    const isoStringPastDay = specificDate.toISOString();
+    let requiredDateLowerLimit = new Date(selectedDate);
+    requiredDateLowerLimit.setUTCHours(0, 0, 0, 0);
+    let requiredDateUpperLimit = new Date(selectedDate);
+    requiredDateUpperLimit.setUTCHours(23, 59, 59, 999);
+    const isoStringLowerLimitDate = requiredDateLowerLimit.toISOString();
+    const isoStringUpperLimitDate = requiredDateUpperLimit.toISOString();
 
-    console.log(
-      "Before sending get request to retrieve daily target",
-      isoStringCurrentDay,isoStringPastDay
-    );
     const fetchDailyTarget = async () => {
       const response = await axios.get(`${backendUrl}/applied-companies`, {
         headers: {
           Authorisation: `Bearer ${token}`,
-          isoStringCurrentDay: isoStringCurrentDay,
-          isoStringPastDay: isoStringPastDay
+        },
+        params: {
+          upperDate: isoStringUpperLimitDate,
+          lowerDate: isoStringLowerLimitDate,
         },
       });
       const data = await response.data;
-      console.log(data.data.length);
-      setDailyTarget(data.data.length);
+      const recievedData = data.companyData;
+      const dailyTargetValue = recievedData.length;
+      console.log("Length of the array recieved : ", dailyTargetValue);
+      setDailyTarget(dailyTargetValue);
+      console.log("Type of the response data", recievedData);
+      setCompanyDetails(recievedData);
+      console.log("Checking the details", companyDetails);
     };
+
     fetchDailyTarget();
   }, []);
   return (
     <div>
-      <div className="flex justify-end my-4">
+      <div className="flex justify-between my-4">
+        <div className="ml-2">
+          <input
+            type="date"
+            name="selectedDate"
+            value={selectedDate}
+            onChange={(event) => {
+              setSelectedDate(event.target.value);
+              console.log("This is the selected date", selectedDate);
+            }}
+          />
+        </div>
         <div className="flex gap-0.5 dark:text-white items-center">
           Daily Target : <span>{dailyTarget}</span>
           <span>/</span>
@@ -71,44 +91,7 @@ const AppliedCompanies = () => {
         </div>
       </div>
       <div className="px-2">
-        <div>
-          <div className="rounded-md bg-[#6A89CC] m-2 py-2 px-4 flex justify-between items-center">
-            <span className="text-white">Today</span>
-            <IoIosArrowDown className="w-8 h-8 fill-white mr-2" />
-          </div>
-          <div className="px-4">
-            <table className="w-full">
-              <thead className="border">
-                <td>Company Name</td>
-                <td>Applied Role</td>
-                <td>Time</td>
-                <td>Other</td>
-              </thead>
-              <tbody className="border">
-                <tr>
-                  <td>Hexaware</td>
-                  <td>Graduate Engineer Trainee</td>
-                  <td>{Date.now()}</td>
-                  <td>No Remark</td>
-                </tr>
-                <tr>
-                  <td>Talent Serve</td>
-                  <td>Full Stack Developer</td>
-                  <td>{Date.now()}</td>
-                  <td>No Remark</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="rounded-md bg-[#6A89CC] m-2 py-2 px-4 flex justify-between items-center">
-          <span className="text-white">Yesterday</span>
-          <IoIosArrowDown className="w-8 h-8 fill-white mr-2" />
-        </div>
-        <div className="rounded-md bg-[#6A89CC] m-2 py-2 px-4 flex justify-between items-center">
-          <span className="text-white">Day before Yesterday</span>
-          <IoIosArrowDown className="w-8 h-8 fill-white mr-2" />
-        </div>
+        <CompanyDetails companyDetails={companyDetails} />
       </div>
     </div>
   );
