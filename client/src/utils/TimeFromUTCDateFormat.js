232e37a0c75acdf17022219befc2cba8a9eef96a
abtyagi15 <abtyagi150702@gmail.com>
2024-06-30 13:10:54 +0530

    Time calculate from ISO String

--- client/src/utils/TimeFromUTCDateFormat.js
+++ client/src/utils/TimeFromUTCDateFormat.js
@@ -1,8 +1,7 @@
 exports.timeExtracterFromUTC = (value) => {
-    const date = new Date(value);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const formatedTime = `${hours}:${minutes}:${seconds}`;
-    return formatedTime;
-}
+  const date = new Date(value);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formatedTime = `${hours}:${minutes}`;
+  return formatedTime;
+};
